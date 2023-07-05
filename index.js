c173496fa94e3528bbee70934c810ba97bb417d9
Kirill Royako <rakot666111@gmail.com>
2023-07-05 18:10:27 +0300

    Test2

---
+++ index.js
@@ -0,0 +1,15 @@
+const myCity = {
+    city: 'New York'
+}
+
+myCity['popular'] = true
+
+console.log(myCity)
+
+const countryPropertyName = 'country'
+myCity [countryPropertyName] = 'USA'
+
+console.log(myCity)
+delete [countryPropertyName]
+
+console.log(myCity)
