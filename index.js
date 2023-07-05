3e587fefe526b3385fdfbd25974ebf1710b6feaf
Kirill Royako <rakot666111@gmail.com>
2023-07-05 18:34:44 +0300

    Global

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myCity = {
+/* const myCity = {
     city: 'New York'
 }
 
@@ -9,7 +9,29 @@ console.log(myCity)
 const countryPropertyName = 'country'
 myCity [countryPropertyName] = 'USA'
 
-console.log(myCity)
-delete [countryPropertyName]
+console.log(myCity) */
 
-console.log(myCity)
+/* const myCity = {
+    city: 'New York',
+    info: {
+        isPopular: true,
+        country: 'USA'
+    }
+}
+
+console.log(myCity.info.isPopular)
+
+delete myCity.info['isPopular']
+
+console.log(myCity) */
+
+const name = 'Kirill'
+const postsQty = 23
+
+const userProfile = {
+    name,
+    postsQty,
+    hasSignedAgreement: false
+}
+
+console.log(userProfile)
