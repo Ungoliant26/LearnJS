710dbf5b674726e4c179aa1fbae0b5d639603062
Kirill Royako <rakot666111@gmail.com>
2023-09-17 21:02:28 +0300

    Sept 17.2

---
+++ modules/moduleOne.mjs
@@ -0,0 +1,6 @@
+const myLoveNum = 7
+const multip = (a, b) => a * b
+
+console.log(myLoveNum)
+
+export default multip
---
+++ modules/moduleTwo.mjs
@@ -0,0 +1,11 @@
+import multipNum from './moduleOne.mjs'
+
+const result1 = multipNum(5, 9)
+
+console.log(result1)
+
+const result2 = multipNum(77, 10)
+
+console.log(result2)
+
+console.log(MyLoveNum)
