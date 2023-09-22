f51669910631c98c7cd0d0fd1bc315c2a69f1941
Kirill Royako <rakot666111@gmail.com>
2023-09-22 11:40:48 +0300

    Last lessonUp

--- modules/modulePromis.js
+++
@@ -1,9 +0,0 @@
-const getData = (url) =>
-    new Promise ((resolve, reject) =>
-        fetch(url)
-            .then(response => response.json())
-            .then(json => resolve(json))
-            .catch(error => reject(error))
-    )
-
-export {getData}
--- modules/moduleThree.mjs
+++
@@ -1,3 +0,0 @@
-const func1 = (a, b) => a / b
-
-export {func1}
